b7bca3df3aba04f3caa0684010d6801d5847468a
miyoku157 <miyoku157@gmail.com>
2016-01-16 20:57:52 +0100

    Cameera correction

--- Advercity/Assets/Script/RotateAround.cs
+++ Advercity/Assets/Script/RotateAround.cs
@@ -4,51 +4,50 @@ using System.Collections;
 public class RotateAround : MonoBehaviour{
 	
 	public float speed = 5.0f;
-	public GameObject terrain;
-	public GameObject xRotator;
 	private float terrainHeight;
-	private Vector2 mouseClickPosition;
-	
+	private float t=0.0f;
 	private void moveRight(){
+		Vector3 pos=transform.position;
 		if(Input.mousePosition.x > Screen.width * 0.95f || Input.GetKey(KeyCode.RightArrow)){
-			this.transform.Translate(Vector3.right * this.speed * Time.deltaTime);
+				this.transform.Translate(Vector3.right * this.speed * Time.deltaTime,Space.World);
 		}
 	}
 	
 	private void moveLeft(){
 		if(Input.mousePosition.x < Screen.width * 0.05f || Input.GetKey(KeyCode.LeftArrow)){
-			this.transform.Translate(-Vector3.right * this.speed * Time.deltaTime);
+			this.transform.Translate(-Vector3.right * this.speed * Time.deltaTime,Space.World);
 		}
 	}
 	
 	private void moveForward(){
 		if(Input.mousePosition.y < Screen.height * 0.05f || Input.GetKey(KeyCode.DownArrow)){
-			this.transform.Translate(-Vector3.up * this.speed * Time.deltaTime);
+			this.transform.Translate(-Vector3.forward * this.speed * Time.deltaTime,Space.World);
 		}
 	}
 	
 	private void moveBackward(){
 		if(Input.mousePosition.y > Screen.height * 0.95f || Input.GetKey(KeyCode.UpArrow)){
-			this.transform.Translate(Vector3.up * this.speed * Time.deltaTime);
+			this.transform.Translate(Vector3.forward * this.speed * Time.deltaTime,Space.World);
 		}
 	}
-	
-	private float ClampAngle(float angle, float min, float max) {
-		if (angle<90 || angle>270){       // if angle in the critic region...
-			if (angle>180) angle -= 360;  // convert all angles to -180..+180
-			if (max>180) max -= 360;
-			if (min>180) min -= 360;
-		}   
-		angle = Mathf.Clamp(angle, min, max);
-		if (angle<0) angle += 360;  // if angle negative, convert to 0..360
-		return angle;
+	private void getTerrainHeight(){
+		RaycastHit hit;
+		if (Physics.Raycast (this.transform.position, this.transform.forward,out hit, 500)) {
+			this.terrainHeight=hit.point.y;
+		}
 	}
-	
+	private void adjustToTerrainHeight(){
+		if (terrainHeight-13  != this.terrainHeight) {
+			this.transform.position = new Vector3 (this.transform.position.x, 13 + terrainHeight, this.transform.position.z);
+		}
+		}
 	public void Update(){
 		this.moveRight();
 		this.moveLeft();
 		this.moveForward();
 		this.moveBackward();
+		getTerrainHeight ();
+		adjustToTerrainHeight ();
 		if(Input.GetKeyDown(KeyCode.Escape)){
 			Application.Quit();
 		}
