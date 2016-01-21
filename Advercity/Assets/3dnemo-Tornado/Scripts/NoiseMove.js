8167aa39a66ea51f007412bbd3247673e574dec9
Exospector <pcheminel@gmail.com>
2016-01-21 23:01:17 +0100

    Add Ninja Animations + Tornado Asset

---
+++ Advercity/Assets/3dnemo-Tornado/Scripts/NoiseMove.js
@@ -0,0 +1,18 @@
+var speed = 1.0;
+var moveSize = Vector3.one;
+private var basePosition : Vector3;
+
+function Start ()
+{
+	basePosition = transform.localPosition;	
+}
+
+function Update ()
+{
+	var offset = Random.GetVector3(speed);
+	offset -= Vector3 (0.5, 0.5, 0.5);
+	offset = Vector3.Scale(moveSize, offset);
+	transform.localPosition = offset + basePosition;
+}
+
+@script AddComponentMenu("Noise/Transform Position")
