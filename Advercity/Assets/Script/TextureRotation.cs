8c3afc18b93b275b227ce308263b36f0328c62ae
Exospector <pcheminel@gmail.com>
2016-01-22 01:28:29 +0100

    Add TextureRotation Script + Fix Crystal Prefab + Add Rain to Main Scene

---
+++ Advercity/Assets/Script/TextureRotation.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextureRotation : MonoBehaviour
+{
+	private Vector2 offset = Vector2.zero;
+	private float speed  = 0.5f;
+	private Renderer rend;
+
+	void Start ()
+	{
+		rend = gameObject.GetComponent<Renderer> ();
+	}
+
+	void Update ()
+	{
+		offset.y = (offset.y + speed * Time.deltaTime) % 1;
+		rend.material.mainTextureOffset = offset;
+	}
+}
